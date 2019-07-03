a49ed2c2304ee259dfdd808afec6fc9759086870
Ryan Jones <ryanjones33@gmail.com>
2019-07-03 10:08:11 -0700

    Create event

--- config/graphqlHttp.js
+++ config/graphqlHttp.js
@@ -1,13 +1,28 @@
 var graphqlHttp = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+var events = []
+
 var graphqlConfig = graphqlHttp({
   schema: buildSchema(`
+      type Event {
+        id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
       type RootQuery {
-          events: [String!]!
+          events: [Event!]!
       }
       type RootMutation {
-          createEvent(name: String): String
+          createEvent(eventInput: EventInput): Event
       }
       schema {
           query: RootQuery
@@ -16,11 +31,18 @@ var graphqlConfig = graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      return events;
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: args.eventInput.date
+      }
+      events.push(event)
+      return event;
     }
   },
   graphiql: true
